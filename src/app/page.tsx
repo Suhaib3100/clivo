87735025df02fbe475c04846d98aaea00e76cfeb
Suhaib3100 <suhaibking310@gmail.com>
2025-09-28 11:16:33 +0530

    Refactor database connection and update Home component to use async function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { prisma } from "@/lib/database";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
-      <Button variant="ghost">
-        Click me
-      </Button>
+
+      
     </div>
 
   );
